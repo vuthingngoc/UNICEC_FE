352a636f4d617cdf5d778084867cedf388711847
vuthingngoc <63228228+vuthingngoc@users.noreply.github.com>
2022-04-26 15:36:22 +0700

    Fix homepage layout and update dashboard

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -14,8 +14,10 @@ function IndexHeader() {
                   <h1 className="display-2 mb-0" style={{ fontWeight: '900', fontFamily: 'sans-serif' }}>
                     UNICEC
                   </h1>
-                  <p className="lead">Nền tảng quản lý thông tin sự kiện và cuộc thi của Câu Lạc Bộ Sinh Viên hàng đầu tại Việt Nam.</p>
-                  <p className="lead">
+                  <p className="lead" style={{ fontFamily: 'sans-serif' }}>
+                    Nền tảng quản lý thông tin sự kiện và cuộc thi của Câu Lạc Bộ Sinh Viên hàng đầu tại Việt Nam.
+                  </p>
+                  <p className="lead" style={{ fontFamily: 'sans-serif' }}>
                     Hỗ trợ tìm kiếm các cuộc thi và sự kiện để mang lại các phần thưởng dành cho người thắng cuộc từ những nhà tài trợ khác nhau.
                   </p>
                   <div className="mt-5">
@@ -34,46 +36,6 @@ function IndexHeader() {
               </Col>
               <Col lg="4">
                 <Row className="pt-5">
-                  {/* <Col md="6">
-                    <Card>
-                      <CardBody>
-                        <div className="icon icon-shape bg-gradient-red text-white rounded-circle shadow mb-4">
-                          <i className="ni ni-active-40" />
-                        </div>
-                        <h5 className="h3">Quản lý câu lạc bộ</h5>
-                        <p>Hỗ trợ quản lý thành viên và hoạt động cho các chủ nhiệm câu lạc bộ.</p>
-                      </CardBody>
-                    </Card>
-                    <Card>
-                      <CardBody>
-                        <div className="icon icon-shape bg-gradient-info text-white rounded-circle shadow mb-4">
-                          <i className="ni ni-active-40" />
-                        </div>
-                        <h5 className="h3">Quản lý cuộc thi</h5>
-                        <p>Hỗ trợ tạo và quản lý các cuộc thi và sự kiện trong và ngoài trường.</p>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                  <Col className="pt-lg-5 pt-4" md="6">
-                    <Card className="mb-4">
-                      <CardBody>
-                        <div className="icon icon-shape bg-gradient-success text-white rounded-circle shadow mb-4">
-                          <i className="ni ni-active-40" />
-                        </div>
-                        <h5 className="h3">Cung cấp thông tin cuộc thi</h5>
-                        <p>Hệ thống thông tin về các cuộc thi từ khắp các trường đại học và doanh nghiệp.</p>
-                      </CardBody>
-                    </Card>
-                    <Card className="mb-4">
-                      <CardBody>
-                        <div className="icon icon-shape bg-gradient-warning text-white rounded-circle shadow mb-4">
-                          <i className="ni ni-active-40" />
-                        </div>
-                        <h5 className="h3">Hỗ trợ thành viên trong câu lạc bộ</h5>
-                        <p>Các thành viên sẽ dễ dàng tiếp cận các thông tin và hoạt đông trong câu lạc bộ.</p>
-                      </CardBody>
-                    </Card>
-                  </Col> */}
                   <img alt="..." src={require('assets/img/brand/mobile_UNICEC.png').default} />
                 </Row>
               </Col>
